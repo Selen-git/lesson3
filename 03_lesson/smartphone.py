2e4fceb0f824c226f78d22ed57412d116e4b8d2f
Selen-git <Selen_6966@inbox.ru>
2025-02-21 00:04:15 +0500

    Update smartphone.py

--- 03_lesson/smartphone.py
+++ 03_lesson/smartphone.py
@@ -1,1 +1,10 @@
-
+class Smartphone :
+    def init(self, brand, model, phone_number) :
+        self.brand = brand
+        self.model = model
+        self.phone_number = phone_number
+from smartphone import Smartphone
+catalog = []
+phone1 = Smartphone("Samsung", "Galaxy S 21", "+79824568932")
+phone2 = Smartphone("Apple", "iPhone", "+79378956314")
+phone3 = Smartphone("Xiaomi", "Redmi Note 10", "+79255894720")
