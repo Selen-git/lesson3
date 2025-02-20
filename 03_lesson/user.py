352b389881ae261f4e564576d253430d2b8e9851
Selen-git <Selen_6966@inbox.ru>
2025-02-20 23:45:03 +0500

    Update user.py

--- 03_lesson/user.py
+++ 03_lesson/user.py
@@ -1,6 +1,7 @@
-class User:
-    def __init__ (self, first_name, last_name):
-    	
-        first_name = ("Олег")
-        last_name = ("Силкин")
-        print(first_name, last_name)
+class User:     
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
