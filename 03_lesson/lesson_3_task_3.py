2c9d652aadeec4fd3ce4c47f164a8891a039986c
Selen-git <Selen_6966@inbox.ru>
2025-02-22 22:33:36 +0500

    Update lesson_3_task_3.py

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -1,1 +1,8 @@
+from adress import Adress
+from mailing import Mailing
 
+to_adress = Adress("623400", "Каменск-Уральский", "Улица Каменская", "12", "24")
+
+from_adress = Adress("620000", "Екатеринбург", "Улица Малышева", "19", "25")
+
+mailing = Mailing(to_adress, from_adress, 500, "TRACK687159")
