b27566406639aa80580b9ec6e530a6665b178449
Selen-git <Selen_6966@inbox.ru>
2025-02-22 21:59:42 +0500

    Update user.py

--- 03_lesson/user.py
+++ 03_lesson/user.py
@@ -1,8 +1,13 @@
 class User:     
-    def __init__(self, first_name: str, last_name: str):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
 
-    def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
-from user import User
+    def print_first_name(self):
+        print("First Name:", self.first_name)
+        
+    def print_last_name(self):
+        print("Last Name:", self.last_name)
+
+    def print_full_name(self):
+        print("Full Name:", self.first_name, self.last_name)
